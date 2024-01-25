88be62501d1de01497049235b89eb3d4b8756e9b
Vedant Banaitkar <78202213+vedantbanaitkar@users.noreply.github.com>
2024-01-25 19:33:28 +0530

    Time: 1 ms (82.31%) | Memory: 42 MB (31.81%) - LeetSync

--- 1944-truncate-sentence/truncate-sentence.java
+++ 1944-truncate-sentence/truncate-sentence.java
@@ -2,17 +2,17 @@ class Solution {
      public static String truncateSentence(String s, int k) {
         int i=0;
         int c=1;
-        String s1 = ""+s.charAt(0);
+        StringBuilder s1 = new StringBuilder(String.valueOf(s.charAt(0)));
         while(c<=k&&i<s.length()-1){
             char ch = s.charAt(i+1);
             if(ch==' '){
                 c++;
             }
             if(c<=k){
-                s1+=ch;
+                s1.append(ch);
             }
             i++;
         }
-        return s1;
+        return s1.toString();
     }
 }
