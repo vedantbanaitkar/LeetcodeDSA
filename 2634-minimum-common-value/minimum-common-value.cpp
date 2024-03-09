013d868f6a70d7449c8adc166a782a3aa6ab0936
Vedant Banaitkar <78202213+vedantbanaitkar@users.noreply.github.com>
2024-03-09 12:00:59 +0530

    Time: 65 ms (80.25%) | Memory: 53 MB (51.54%) - LeetSync

--- 2634-minimum-common-value/minimum-common-value.cpp
+++ 2634-minimum-common-value/minimum-common-value.cpp
@@ -3,10 +3,8 @@ public:
     int getCommon(vector<int>& arr1, vector<int>& arr2) {
         int i=0; 
         int j=0;
-        int l1=arr1.size();
-        int l2=arr2.size();
         
-        while(i<l1 && j<l2){
+        while(i<arr1.size() && j<arr2.size()){
             if(arr1[i]==arr2[j]){
                 return arr1[i];
             }
