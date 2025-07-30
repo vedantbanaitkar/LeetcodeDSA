f5ffb7813c69508394ffa7a8a73474fbf577bd9a
Vedant Banaitkar <vedantbanaitkar@gmail.com>
2025-07-30 16:36:34 +0530

    Time: 2 ms (73.51%) | Memory: 42.6 MB (23.42%) - LeetSync

--- 3242-count-elements-with-maximum-frequency/count-elements-with-maximum-frequency.java
+++ 3242-count-elements-with-maximum-frequency/count-elements-with-maximum-frequency.java
@@ -1,28 +1,24 @@
+import java.util.*;
 class Solution {
-    public int maxFrequencyElements(int[] nums) {
-      int maxNum = Integer.MIN_VALUE;
-      for(int num:nums){
-         if(num>maxNum) maxNum = num;
-      }
-       
-       int arr[] = new int[maxNum+1];
-       for(int num2:nums){
-           arr[num2]++;
-       }
-
-        int maxFrequency = 0;
-        for (int freq : arr) {
-            if (freq > maxFrequency) {
-                maxFrequency = freq;
+    public int maxFrequencyElements(int[] arr) {
+        HashMap<Integer, Integer> mp = new HashMap<>();
+        for(int i=0; i<arr.length; i++){
+            mp.put(arr[i], mp.getOrDefault(arr[i], 0)+1);
+        }
+        int max=-1;
+        for(int key: mp.keySet()){
+            int k = mp.get(key);
+            if(k>max){
+                max = k;
             }
         }
-
-         int countMaxFrequency = 0;
-        for (int freq : arr) {
-            if (freq == maxFrequency) {
-                countMaxFrequency++;
+        int s=0;
+        for(int key: mp.keySet()){
+            int t = mp.get(key);
+            if(t==max){
+                s+=max;
             }
         }
-    return maxFrequency*countMaxFrequency;
-}
+        return s;
+    }
 }
